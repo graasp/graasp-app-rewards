fbbaa74c73be6f4ccdc731cfeeb76a20dbbb88ad
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2019-12-11 16:31:58 +0100

    feat: display trophies in grid

--- src/components/modes/student/MyBadges.js
+++ src/components/modes/student/MyBadges.js
@@ -3,18 +3,31 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Typography from '@material-ui/core/Typography';
-import { Table } from 'reactstrap';
-import './MyBadges.css';
+import { Typography, Grid, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const renderBadges = props => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginTop: theme.spacing(2),
+  },
+  paper: {
+    height: 200,
+    width: 200,
+  },
+  grid: {
+    flexGrow: 1,
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const renderBadges = (props, classes) => {
   const { t, badges, appInstanceResources } = props;
   // if there are no resources, show an empty table
   if (!appInstanceResources.length) {
     return (
-      <tr>
-        <td colSpan={5}>{t('No badges have been assigned to you yet')}</td>
-      </tr>
+      <Typography variant="h4">
+        {t('No badges have been assigned to you yet')}
+      </Typography>
     );
   }
   // map each app instance resource to a row in the table
@@ -28,28 +41,25 @@ const renderBadges = props => {
 
     // return a row for this entry
     return (
-      <tr key={_id}>
-        <td>
+      <Grid item key={_id}>
+        <Paper className={classes.paper}>
           <FontAwesomeIcon color={color} icon={icon} size="10x" />
-          <Typography variant="h5">{t(label)}</Typography>
-        </td>
-      </tr>
+          <Typography variant="h6">{t(label)}</Typography>
+        </Paper>
+      </Grid>
     );
   });
 };
 
 const MyBadges = props => {
   const { t } = props;
+  const classes = useStyles();
   return (
-    <div className="AssignedBadges">
-      <Table>
-        <thead>
-          <tr>
-            <th>{t('Trophy Room')}</th>
-          </tr>
-        </thead>
-        <tbody>{renderBadges(props)}</tbody>
-      </Table>
+    <div className={classes.root}>
+      <Typography variant="h2">{t('Trophy Room')}</Typography>
+      <Grid container justify="center" spacing={2} className={classes.grid}>
+        {renderBadges(props, classes)}
+      </Grid>
     </div>
   );
 };
